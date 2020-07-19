ca2dde5f9fe7cf07e38615d42dde841f7b8b0ea3
harkirat singh <singhharkirat051@gmail.com>
2020-07-19 14:49:41 -0400

    Initial commit

---
+++ app/src/main/java/com/example/tovisit_harkirat_c0772087_android/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.tovisit_harkirat_c0772087_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
